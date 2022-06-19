3e3329cd2148f8a80d8c90fee9ea08c64aeee4ce
bil-bal <bilal.balguen@gmx.de>
2022-06-19 19:55:24 +0200

    change router to hashrouter

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import './App.css';
 import Header from "./components/Header"
 import ProductListing from './components/ProductListing';
 import ResultSummary from './components/ResultSummary';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
 
 function App() {  
   return (
     <div className="App">
-      <Router>
+      <HashRouter>
         <Header />
         <Routes>
           <Route path="/" element={<ProductListing/>} />
           <Route path="/result/:urlCode" element={<ResultSummary/>} />
           <Route>404 Not Found</Route>
         </Routes>      
-      </Router>
+      </HashRouter>
     </div>
   );
 }
